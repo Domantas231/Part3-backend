c5bbc8d700eddd0bab446e1efddfbc8349235b53
Domantas Bieliūnas <domantas231@gmail.com>
2025-07-02 21:02:25 +0300

    add: frontend

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ morgan.token('data', function (req) { return JSON.stringify(req.body) })
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms -- :data', {
     skip: function (req, res) { return req.method !== 'POST' } 
 }))
