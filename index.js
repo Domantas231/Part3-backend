cbd40c4cda0130f40e70b92a00e244074ff6e531
Domantas Bieliūnas <domantas231@gmail.com>
2025-07-04 17:11:01 +0300

    uibuild

--- index.js
+++ index.js
@@ -11,18 +11,18 @@ morgan.token('data', function (req) { return JSON.stringify(req.body) })
 app.use(express.static('dist'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms -- :data', {
-    skip: function (req, res) { return req.method !== 'POST' } 
+  skip: function (req) { return req.method !== 'POST' }
 }))
 app.use(morgan('tiny', {
-    skip: function (req, res) { return req.method === 'POST' } 
+  skip: function (req) { return req.method === 'POST' }
 }))
 
 app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
   if (!body.name || !body.number) {
-    return response.status(400).json({ 
-      error: 'content missing' 
+    return response.status(400).json({
+      error: 'content missing'
     })
   }
 
@@ -33,8 +33,7 @@ app.post('/api/persons', (request, response, next) => {
 
   person.save().then(savedPerson => {
     response.json(savedPerson)
-  })
-  .catch(error => next(error))
+  }).catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -64,7 +63,7 @@ app.get('/api/persons', (request, response) => {
 
 app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
-    .then(result => {
+    .then(() => {
       response.status(204).end()
     })
     .catch(error => next(error))
@@ -73,12 +72,12 @@ app.delete('/api/persons/:id', (request, response, next) => {
 app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id).then(person => {
     if (person) {
-        response.json(person)
-      } else {
-        response.status(404).end()
-      }
-  })
-  .catch(error => next(error))
+      response.json(person)
+    }
+    else {
+      response.status(404).end()
+    }
+  }).catch(error => next(error))
 })
 
 const PORT = process.env.PORT
